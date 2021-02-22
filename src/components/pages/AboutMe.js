9639630d53116e5d37f51e54107378ee2f6918c8
Ethan Jackson <ethantjackson44@gmail.com>
2021-02-21 18:36:10 -0800

    updated about me and contact me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -12,7 +12,7 @@ export default function AboutMe() {
         <div class="about-me-images"><img className="about-me-images" src={Ucr} alt="ucr" height="400"></img></div>
       </div>
       <p className='about'>
-        &nbsp;&nbsp;&nbsp;&nbsp; Hello, my name is Ethan Jackson, and I'm an aspiring software engineer at the University of California, Riverside. My areas of study primarily lie in C++, but I have also ventured into Java and Javascript, utilizing redux and react (which I used to make this website!). Regarding C++, I have utilized graphics libraries including SFML and QT to create several games and applications. Outside of coding, I enjoy making music, and I play trumpet in several classical and jazz ensembles. In general, I am always looking for opportunities to learn and expand my skillsets!
+        &nbsp;&nbsp;&nbsp;&nbsp; Hello, my name is Ethan Jackson, and I'm an aspiring software engineer at the University of California, Riverside. My areas of study primarily lie in C++, but I have also ventured into Java and Javascript, utilizing redux and react (which I used to make this website!). Regarding C++, I have utilized graphics libraries including SFML and QT to create several games and applications. Outside of coding, I enjoy making music, and I play trumpet in several classical and jazz ensembles. I am always looking for opportunities to learn and expand my skillsets!
       </p>
     </div>
   );
